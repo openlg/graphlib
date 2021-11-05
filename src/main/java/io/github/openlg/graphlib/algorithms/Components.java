6222a63b1d6e13bfcf5f3f37fd32519cd5f91a91
Leon <lirufei0808@gmail.com>
2021-11-05 15:29:10 +0800

    add finds all connected components in a graph algorithms

---
+++ src/main/java/io/github/openlg/graphlib/algorithms/Components.java
@@ -0,0 +1,58 @@
+package io.github.openlg.graphlib.algorithms;
+
+import io.github.openlg.graphlib.Graph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author lg<lirufei0808 @ gmail.com>
+ * @date 2021/11/5 下午2:16
+ * @description
+ */
+public class Components {
+
+    /**
+     * Finds all connected components in a graph and returns an array of these components. Each component is itself an array that contains the ids of nodes in the component.
+     *
+     * This function takes O(|V|) time.
+     * @param graph graph
+     * @param <N>   node data type
+     * @param <E>   edge data type
+     * @return all connected components
+     */
+    public <N, E> List<Graph<N, E>> getComponents(Graph<N, E> graph) {
+
+        Map<String, Boolean> visited = new HashMap<>();
+        List<Graph<N, E>> graphs = new ArrayList<>();
+
+        graph.getNodes().forEach(nodeId -> {
+            Graph<N, E> component = new Graph<>();
+            findComponent(visited, graph, nodeId, component);
+            if (component.nodeCount() > 0) {
+                graphs.add(component);
+            }
+        });
+        return graphs;
+    }
+
+    private <N, E> void findComponent(Map<String, Boolean> visited, Graph<N, E> graph, String nodeId, Graph<N, E> component) {
+        if (visited.containsKey(nodeId))
+            return;
+        visited.put(nodeId, true);
+
+        component.setNode(nodeId, graph.getNode(nodeId));
+
+        graph.successors(nodeId).forEach(successor -> {
+            findComponent(visited, graph, successor, component);
+            component.setEdge(nodeId, successor, graph.getEdge(nodeId, successor));
+        });
+        graph.predecessors(nodeId).forEach(predecessor -> {
+            findComponent(visited, graph, predecessor, component);
+            component.setEdge(predecessor, nodeId, graph.getEdge(predecessor, nodeId));
+        });
+    }
+
+}
---
+++ src/test/java/io/github/openlg/graph/alg/TestComponents.java
@@ -0,0 +1,54 @@
+package io.github.openlg.graph.alg;
+
+import io.github.openlg.graphlib.Graph;
+import io.github.openlg.graphlib.algorithms.Components;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * @author lg<lirufei0808 @ gmail.com>
+ * @date 2021/11/5 下午2:51
+ * @description
+ */
+public class TestComponents {
+
+    @Test
+    public void test() {
+        // returns an empty list for an empty graph
+        Graph g = new Graph();
+        Assert.assertEquals(new Components().getComponents(g).size(), 0);
+
+        // returns singleton lists for unconnected nodes
+        g.setNode("a");
+        g.setNode("b");
+
+        List<Graph> graphList = new Components().getComponents(g);
+        Assert.assertEquals(graphList.size(), 2);
+        Assert.assertEquals(graphList.get(0).nodeCount(), 1);
+        Assert.assertEquals(graphList.get(0).edgeCount(), 0);
+        Assert.assertEquals(graphList.get(1).nodeCount(), 1);
+        Assert.assertEquals(graphList.get(0).edgeCount(), 0);
+
+        // returns a list of nodes in a component
+        g = new Graph();
+        g.setEdge("a", "b");
+        g.setEdge("b", "c");
+        graphList = new Components().getComponents(g);
+        Assert.assertEquals(graphList.size(), 1);
+        Assert.assertArrayEquals(graphList.get(0).getNodes().toArray(), new String[]{"a", "b", "c"});
+
+        // returns nodes connected by a neighbor relationship in a digraph
+        g = new Graph();
+        g.setPath("a", "b", "c", "a");
+        g.setEdge("d", "c");
+        g.setEdge("e", "f");
+        graphList = new Components().getComponents(g);
+        Assert.assertEquals(graphList.size(), 2);
+        Assert.assertEquals(graphList.get(0).getNodes().toArray(), new String[]{"a", "b", "c", "d"});
+        Assert.assertEquals(graphList.get(1).getNodes().toArray(), new String[]{"e", "f"});
+
+    }
+
+}
